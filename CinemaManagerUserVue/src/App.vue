3f8e0a144a1b18ff5ab6e1682ee003b1fab8772b
沉着冷静 <2478261806@qq.com>
2024-09-04 15:01:02 +0800

    电影购票系统 user-前端

---
+++ CinemaManagerUserVue/src/App.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Description: 
+ * @Author: 
+ * @Date: 2024-02-24 18:08:34
+-->
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import './assets/css/global.css'
+import '@/assets/font/font.css'
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+
+</style>
